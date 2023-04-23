af96b8dc2cb2992b416f5b1d0bb35cb55701fa34
Drewman5000 <andrewrhyland@gmail.com>
2023-04-23 11:21:55 -0700

    deleted:    loadPartials.js

--- loadPartials.js
+++
@@ -1,8 +0,0 @@
-function loadPartialHTML_(partial) {
-    const htmlServ = HtmlService.createTemplateFromFile(partial);
-    return htmlServ.evaluate().getContent();
-  }
-  
-  function loadSearchView() {
-    return loadPartialHTML_("search");
-  }
